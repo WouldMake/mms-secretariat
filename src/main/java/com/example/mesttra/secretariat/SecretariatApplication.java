3f2030c7b7ff338d37dcb3b7fa83cd8c722b44aa
Marcelo Faria <marcelo.fari@gmail.com>
2021-09-02 10:51:27 -0300

    Initial commit
     Changes to be committed:
    	new file:   .gitignore
    	new file:   pom.xml
    	new file:   src/main/java/com/example/mesttra/secretariat/SecretariatApplication.java
    	new file:   src/main/resources/application.properties
    	new file:   src/test/java/com/example/mesttra/secretariat/SecretariatApplicationTests.java

---
+++ src/main/java/com/example/mesttra/secretariat/SecretariatApplication.java
@@ -0,0 +1,13 @@
+package com.example.mesttra.secretariat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecretariatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecretariatApplication.class, args);
+	}
+
+}
