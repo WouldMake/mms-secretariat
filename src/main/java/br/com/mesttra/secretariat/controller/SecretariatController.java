d258683256b135eb87dc3c5400fe40c9c4607c53
Marcelo Faria <marcelo.fari@gmail.com>
2021-09-20 17:56:45 -0300

    Inclusão de documentação com o Swagger.
     Changes to be committed:
    	modified:   pom.xml
    	new file:   src/main/java/br/com/mesttra/secretariat/config/SpringFoxConfig.java
    	modified:   src/main/java/br/com/mesttra/secretariat/controller/SecretariatController.java

--- src/main/java/br/com/mesttra/secretariat/controller/SecretariatController.java
+++ src/main/java/br/com/mesttra/secretariat/controller/SecretariatController.java
@@ -4,6 +4,9 @@ import br.com.mesttra.secretariat.exception.BusinessException;
 import br.com.mesttra.secretariat.model.Secretariat;
 import br.com.mesttra.secretariat.request.ChangeInvestigationRequest;
 import br.com.mesttra.secretariat.service.SecretariatService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,15 +24,39 @@ public class SecretariatController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @ApiOperation(value = "Add secretariat",
+            notes = "This method adds a new secretariat.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "Secretariat added"),
+            @ApiResponse(code = 500, message = "Internal Error"),
+    })
     public Secretariat addSecretariat(@Valid @RequestBody Secretariat secretariat) { return this.secretariatService.addSecretariat(secretariat); }
 
     @GetMapping
+    @ApiOperation(value = "List secretariats",
+            notes = "This method lists all secretariats.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Secretariats listed"),
+            @ApiResponse(code = 500, message = "Internal Error"),
+    })
     public List<Secretariat> listSecretariats() { return this.secretariatService.listSecretariats(); }
 
     @GetMapping("/{id}")
+    @ApiOperation(value = "Find secretariat by id",
+            notes = "This method finds a secretariat by its id and returns secretariat data.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Secretariat found"),
+            @ApiResponse(code = 500, message = "Internal Error"),
+    })
     public Optional<Secretariat> findSecretariat(@PathVariable Long id) { return this.secretariatService.findSecretariat(id); }
 
     @PatchMapping("{id}/investigation")
+    @ApiOperation(value = "Change secretariat investigation status",
+            notes = "This method finds a secretariat by its id and changes the investigation status.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Secretariat changed"),
+            @ApiResponse(code = 500, message = "Internal Error"),
+    })
     public Secretariat changeInvestigation(@PathVariable Long id, @Valid @RequestBody ChangeInvestigationRequest changeInvestigationRequest) throws BusinessException {
         return this.secretariatService.changeInvestigation(id, changeInvestigationRequest);
     }
